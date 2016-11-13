de1483ef4e753e7d12bcc56307b17cdb68c829c2
Tom Reznick <thomas.reznick@gmail.com>
2016-11-13 09:47:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Trump to Alec Baldwin
+Because it makes the world slightly better
---
+++ trump-to-alec-baldwin.js
@@ -0,0 +1,4 @@
+(function(){
+  document.body.innerHTML = document.body.innerHTML
+    .replace(/((D|d)onald( (J|j)\.)? )?(T|t)rump/g, "Alec Baldwin");
+})();
